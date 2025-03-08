fc95a90edbe7ce99c2b2a2b93119efc9de9d0ab5
Aescosaurus <michaelbuslik@hotmail.com>
2025-03-08 08:24:02 -0700

    Return ClearUnusedGhosts call & add comment for clarification

--- Scripts/Area/AreaManager.js
+++ Scripts/Area/AreaManager.js
@@ -207,7 +207,7 @@ class AreaManager
 	
 	LoadMap()
 	{
-		// this.playerManager.ClearUnusedGhosts()
+		this.playerManager.ClearUnusedGhosts() // this is needed for char select area
 		this.neutralEntities.length = 0
 		
 		this.map.SetTileSprSht( this.areas[this.curArea].GetTileSheet() )
